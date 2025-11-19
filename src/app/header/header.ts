1ab8336247dc6644674d18e8514019f2ade8783d
Travline <poluxmorales1@gmail.com>
2025-11-19 17:08:38 -0500

    header works and buttons standard

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,21 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { catchError, Observable, of } from 'rxjs';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { SearchService } from '@app/searching/search-service';
 import { LucideAngularModule, Hamburger } from 'lucide-angular';
+import { Router, RouterLink } from '@angular/router';
+import { SKIP_GLOBAL_AUTH_REDIRECT } from '@app/api-error.interceptor';
 
 // Define la estructura de la respuesta exitosa
 interface HealthResponse {
-  message: string;
+  me: string;
 }
 
 type ApiResponse = HealthResponse | ErrorState;
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, ReactiveFormsModule, LucideAngularModule],
+  imports: [CommonModule, ReactiveFormsModule, LucideAngularModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -35,11 +37,26 @@ export class Header {
 
   public searchControl = new FormControl('');
 
+    url: string = '';
+  
+  constructor(private router: Router) { 
+
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.url = this.router.url.split('/')[1] || '';
+    });
+  } 
+
+  private context = new HttpContext().set(SKIP_GLOBAL_AUTH_REDIRECT, true)
   private http = inject(HttpClient);
   res$: Observable<ApiResponse> = this.http.get<HealthResponse>(
-    'https://fororataback.onrender.com/health',
+    'https://fororataback.onrender.com/me',
     {
-      headers: { 'accept': 'application/json' }
+      headers: { 'accept': 'application/json' },
+      withCredentials: true,
+      context: this.context
     }
   ).pipe(
     catchError((err: { status: number }) => {
